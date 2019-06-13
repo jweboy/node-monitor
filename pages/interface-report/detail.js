fdf41546832f2bc1671b6148d29db3eb2f2028a7
jweboy <1259524459@qq.com>
2019-06-13 16:43:55 +0800

    :lipstick: Adjust the way ant css file is introduced

--- pages/interface-report/detail.js
+++ pages/interface-report/detail.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Descriptions, Button, Icon } from 'antd';
 import PropTypes from 'prop-types';
 import Router from 'next/router';
-import 'antd/dist/antd.css';
+// import { format } from 'jweboy-utils/lib/date-time';
+import dayjs from 'dayjs';
 import './index.css';
 
 const DescriptionItem = Descriptions.Item;
