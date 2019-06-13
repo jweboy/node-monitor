479de9806c156fafc9c478de2a1270424cd831e9
jweboy <1259524459@qq.com>
2019-06-13 15:19:53 +0800

    :art: improved `logger` and `timeFormat`

--- pages/interface-report/config.js
+++ pages/interface-report/config.js
@@ -1,31 +1,31 @@
 // import { dateTime } from 'jweboy-utils'
 // const { format } = dateTime
-import dayjs from 'dayjs'
+import dayjs from 'dayjs';
 
 export const columns = [
-    {
-        title: '请求方法',
-        key: 'method',
-        dataIndex: 'method',
-    },
-    {
-        title: '状态码',
-        key: 'code',
-        dataIndex: 'code',
-    },
-    {
-        title: '请求路径',
-        key: 'url',
-        dataIndex: 'url',
-    },
-    {
-        title: '创建时间',
-        key: 'createAt',
-        dataIndex: 'createAt',
-        render(value) {
-            return (
-                <span>{dayjs(value).format('YYYY-MM-DD  HH:mm:ss')}</span>
-            )
-        },
-    }
-]
+	{
+		title: '请求方法',
+		key: 'method',
+		dataIndex: 'method',
+	},
+	{
+		title: '状态码',
+		key: 'code',
+		dataIndex: 'code',
+	},
+	{
+		title: '请求路径',
+		key: 'url',
+		dataIndex: 'url',
+	},
+	{
+		title: '创建时间',
+		key: 'createAt',
+		dataIndex: 'createAt',
+		render(value) {
+			return (
+				<span>{dayjs(value).format('YYYY-MM-DD  HH:mm:ss')}</span>
+			);
+		},
+	}
+];
