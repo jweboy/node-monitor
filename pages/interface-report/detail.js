3e73cfffd7001ceb7a85ccbace58aa0d758152e0
jweboy <1259524459@qq.com>
2019-07-02 16:19:44 +0800

    :construction: Update interface detail page display fields

--- pages/interface-report/detail.js
+++ pages/interface-report/detail.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Descriptions, Button, Icon, Row, Col, Badge, Tag, Breadcrumb } from 'antd';
+import { Icon, Row, Col, Badge, Tag, Breadcrumb } from 'antd';
 import PropTypes from 'prop-types';
-import Router from 'next/router';
 // import { format } from 'jweboy-utils/lib/date-time';
 import dayjs from 'dayjs';
 import { statusMap,  methodColorMap } from './config';
 import Layout from '../../components/Layout';
 import './index.css';
+
 
 class InterfaceDetail extends Component {
     static defaultProps = {
@@ -34,6 +34,7 @@ class InterfaceDetail extends Component {
     	const { data } = this.props;
     	const currentStatus = statusMap[data.status];
     	const currMethodColorText = methodColorMap[data.method.toLowerCase()];
+    	const isSucceed = data.status === 'succeed';
 
     	return (
     		<Layout>
@@ -50,43 +51,59 @@ class InterfaceDetail extends Component {
     				<h2 className="interface-title">基本信息</h2>
     				<div className="interface-info">
     					<Row className="interface-row">
-    						<Col span="3">接口路径：</Col>
-    						<Col span="9">
+    						<Col span={3}>接口路径：</Col>
+    						<Col span={9}>
     							<Tag color={currMethodColorText}>
     								{data.method}
     							</Tag>
     							<span>{data.url}</span>
     						</Col>
-    						<Col span="3">创建时间：</Col>
-    						<Col span="9">{dayjs(data.createAt).format('YYYY-MM-DD HH:mm:ss')}</Col>
+    						<Col span={3}>创建时间：</Col>
+    						<Col span={9}>{dayjs(data.createAt).format('YYYY-MM-DD HH:mm:ss')}</Col>
     					</Row>
     					<Row className="interface-row">
-    						<Col span="3">状态：</Col>
-    						<Col span="9">
+    						<Col span={3}>状态：</Col>
+    						<Col span={9}>
     							<Badge {...currentStatus} />
     						</Col>
-    						<Col span="3">状态码：</Col>
-    						<Col span="9">{data.code}</Col>
+    						{
+    							isSucceed ? (
+    								<div>
+    									<Col span={3}>请求时长：</Col>
+    									<Col span={9}>
+    										{`${parseInt(data.request)}ms`}
+    									</Col>
+    								</div>
+    							) : (
+    								<div>
+    									<Col span={3}>状态码：</Col>
+    									<Col span={9}>{data.code}</Col>
+    								</div>
+    							)
+    						}
     					</Row>
     					<Row className="interface-row">
-    						<Col span="3">浏览器内核：</Col>
-    						<Col span="21">{data.browser}</Col>
+    						<Col span={3}>浏览器内核：</Col>
+    						<Col span={21}>{data.browser}</Col>
     					</Row>
     				</div>
     			</div>
-    			<div>
-    				<h2 className="interface-title">错误信息</h2>
-    				<div className="interface-info">
-    					<Row className="interface-row">
-    						{data.message}
-    					</Row>
-    				</div>
-    			</div>
+    			{
+    				!isSucceed &&
+                    <div>
+                    	<h2 className="interface-title">错误信息</h2>
+                    	<div className="interface-info">
+                    		<Row className="interface-row">
+                    			{data.message}
+                    		</Row>
+                    	</div>
+                    </div>
+    			}
     			<div>
     				<h2 className="interface-title">请求参数</h2>
     				<div className="interface-info">
     					<Row className="interface-row">
-    						{data.info}
+    						{JSON.parse(data.info)}
     					</Row>
     				</div>
     			</div>
